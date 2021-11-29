16b53cb5caad709b3f48042db2c2cd302dd28cde
En Zhou <zhouen.nathan@gmail.com>
2021-11-30 00:56:20 +0200

    Complete assignment-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopping-app';
+    showSecret = false;
+    log = [];
+    onToggleDetails() {
+        this.showSecret = !this.showSecret;
+        // this.log.push(this.log.length + 1);
+        this.log.push(new Date());
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
-import { FormsModule } from "@angular/forms";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ServerComponent,
-        ServersComponent,
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
