da8f6364b9748672336d097bac6ec22907bb34cc
En Zhou <zhouen.nathan@gmail.com>
2021-12-01 20:05:56 +0200

    Start section 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopping-app';
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
